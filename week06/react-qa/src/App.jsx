394ae0d92eb796ad8351339839204db8fa7be8da
andr3apl <andreapala32@gmail.com>
2024-05-23 18:16:32 +0200

    first step react e jsx

--- week06/react-qa/src/App.jsx
+++ week06/react-qa/src/App.jsx
@@ -9,25 +9,22 @@ function Answer(text, respondent, score, date) {
   this.respondent = respondent;
   this.score = score;
   this.date = date;
-  this.str = function () { return `${this.text} (by ${this.respondent}) on ${this.date.format('YYYY-MM-DD')}, score: ${this.score}` }
+  this.str = function () { return `${this.text} (by ${this.respondent})  on ${this.date.format('YYYY-MM-DD')}, score: ${this.score}` };
 }
-
 
 const answerList = [
   new Answer('for of', 'Alice', 3, dayjs('2023-03-07')),
-  new Answer('for i=0,i<N,i++', 'Harry', 1, dayjs('2023-03-04')),
+  new Answer('for i=0, i<N, i++', 'Harry', 1, dayjs('2023-03-04')),
   new Answer('for in', 'Harry', -2, dayjs('2023-03-02')),
-  new Answer('i=0 while(i<N)', 'Carol', -1, dayjs('2023-03-01')),
+  new Answer('i=0 while (i<N)', 'Carol', -1, dayjs('2023-03-01')),
 ];
 
 function MyHeader(props) {
-	return (
-		<header>
-			<h1>{props.appName || "HeapOverrun"}</h1>
-		</header>
-	);
+  return (
+    <header>
+      <h1>{props.appName || "HeapOverrun"}</h1>
+    </header>);
 }
-
 
 function MyFooter(props) {
   return (<footer>
@@ -39,7 +36,7 @@ function MyFooter(props) {
 function MyTable(props) {
   return (
     <Table>
-      {/* <Table striped bordered hover> */}
+      { }
       <thead>
         <tr>
           <th>Date</th>
@@ -50,45 +47,46 @@ function MyTable(props) {
         </tr>
       </thead>
       <tbody>
-				{props.listOfAnswers.map( (e,index) => 
-				 <tr key={index}>
+        {props.listOfAnswers.map((e, index) =>
+          <tr key={index}>
             <td>{e.date.format("YYYY-MM-DD")}</td>
             <td>{e.text}</td>
             <td>{e.respondent}</td>
             <td>{e.score}</td>
             <td><Button variant="primary">Vote</Button></td>
           </tr>)}
+
         <tr>
           <td><Form.Control type="date" name="date" /></td>
           <td><Form.Control type="text" name="text" /></td>
           <td><Form.Control type="text" name="respondent" /></td>
           <td><Form.Control className="w-25" type="text" name="score" /></td>
-          <td><Button variant="primary">+</Button> </td>
+          <td><Button variant="primary">+</Button></td>
         </tr>
       </tbody>
     </Table>
-  )
+  );
 }
 
-
-function Main(props) {
-  return (<>
-    <Row>
-      <Col xs={9}>
-        <p>Question: Best way of enumerating an array in JS</p>
-      </Col>
-      <Col xs={3}>
-        <p>Author: Enrico</p>
-      </Col>
-    </Row>
-    <Row>
-      <h2>Current Answers</h2>
-    </Row>
-    <Row>
-      <MyTable listOfAnswers={answerList} />
-    </Row>
-  </>
-  );
+function Main() {
+  return (
+    <>
+      <Row>
+        <Col xs={9}>
+          <p>Question : Best way of numerating an array in JS</p>
+        </Col>
+        <Col xs={3}>
+          <p>Author : Andrea</p>
+        </Col>
+      </Row>
+      <Row>
+        <h2>Current Answer</h2>
+      </Row>
+      <Row>
+        <MyTable listOfAnswers={answerList} />
+      </Row>
+    </>
+  )
 }
 
 function App() {
@@ -99,7 +97,7 @@ function App() {
       <Main />
       <MyFooter />
     </Container>
-  )
+  );
 }
 
 export default App
